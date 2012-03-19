b908d43fa8de1da8303cbe97ce2c88c6e5b0a42b
Jaime Huerta-Cepas <jhcepas@gmail.com>
2012-03-19 14:02:10 +0100

    some changes in update_db

--- update_taxadb.py
+++ update_taxadb.py
@@ -6,6 +6,7 @@ def load_ncbi_tree_for_dump():
     parent2child = {}
     name2node = {}
     node2taxname = {}
+    print "Loading node names..."
     for line in open("names.dmp"):
         fields =  map(strip, line.split("|"))
         nodename = fields[0]
@@ -13,9 +14,9 @@ def load_ncbi_tree_for_dump():
         taxname = fields[1]
         if name_type == "scientific name":
             node2taxname[nodename] = taxname.lower() 
-
-    print len(node2taxname), "names loaded"
-
+    print len(node2taxname), "names loaded."
+    
+    print "Loading nodes..."
     for line in open("nodes.dmp"):
         fields =  line.split("|")
         nodename = fields[0].strip().lower()
@@ -25,9 +26,9 @@ def load_ncbi_tree_for_dump():
         n.taxname = node2taxname[nodename]
         parent2child[nodename] = parentname
         name2node[nodename] = n
+    print len(name2node), "nodes loaded."
 
-    print len(name2node), "nodes loaded"
-
+    print "Linking nodes..."
     for node in name2node:
        if node == "1":
            t = name2node[node]
@@ -35,13 +36,14 @@ def load_ncbi_tree_for_dump():
            parent = parent2child[node]
            parent_node = name2node[parent]
            parent_node.add_child(name2node[node])
+    print "Tree is loaded."
     return t
 
 def generate_table(t):
     OUT = open("taxa.tab", "w")
     for j, n in enumerate(t.traverse()):
         if j%1000 == 0:
-            print "\r",j,
+            print "\r",j,"nodes inserted into the DB.",
         temp_node = n
         track = []
         while temp_node:
@@ -54,12 +56,20 @@ def generate_table(t):
     OUT.close()
 
 t = load_ncbi_tree_for_dump()
-print "printing:",
+
+print "Updating database..."
 generate_table(t)
-print """ 
- # sqlite3 taxa.sqlite
- create table species
- (taxid INT PRIMARY KEY, parent INT, spname VARCHAR(50), track TEXT);
-  .separator "\t"
-  .import taxa.tab species
+CMD = open("commands.tmp", "w")
+cmd = """
+DROP TABLE IF EXISTS species; 
+CREATE TABLE species (taxid INT PRIMARY KEY, parent INT, spname VARCHAR(50), track TEXT);
+.separator "\t"
+.import taxa.tab species
 """
+CMD.write(cmd)
+CMD.close()
+os.system("sqlite3 taxa.sqlite < commands.tmp")
+
+print "Creating extended newick file with the whole NCBI tree [ncbi.nw]"
+t.write(outfile="ncbi.nw", features=["name", "taxname"])
+
